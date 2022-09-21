97c4e835849ea1f43b71569234ea520d8bf0e859
leonardo <105162646+Castillojavi@users.noreply.github.com>
2022-09-21 18:01:57 -0300

    criando pasta routes para rotas de acesso
    adicionando rotas de acesso pra fucion do CRUD

---
+++ routes/professor.js
@@ -0,0 +1,17 @@
+const express = require ('express');
+const router = express.Router();
+// importa controlador 'apiController.js' da pasta: 
+// ‘../controllers/apiController’
+const apiController = require('../controllers/professor');
+// url do teste será: http://localhost:5000/api/teste
+
+router.get('/teste', apiController.test);
+// TODO: listar pontos de interesse da BD
+router.get('/details',apiController.details);
+// TODO: adicionar novo ponto de interesse
+router.post('/interest',apiController.create);
+// TODO: atualizar ponto de interesse
+router.put('/interest/:id',apiController.update);
+// TODO: apagar ponto de interesse
+router.delete('/interest/:id',apiController.delete);
+module.exports = router;
